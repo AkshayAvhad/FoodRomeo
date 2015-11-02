654342c0b5add2e1d5f04a095cbaec0efb0130d5
Akshay Avhad <akshayavhad9@gmail.com>
2015-11-02 18:10:22 +0530

    Sir I'm not getting where is the error.

--- app/src/main/java/com/tiltedplay/foodromeo/splashscreen/PaginationLayout.java
+++ app/src/main/java/com/tiltedplay/foodromeo/splashscreen/PaginationLayout.java
@@ -1,18 +1,11 @@
 package com.tiltedplay.foodromeo.splashscreen;
 
-import android.app.ActionBar;
-import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.HorizontalScrollView;
-import android.widget.LinearLayout;
 import android.widget.ViewFlipper;
 
 import com.tiltedplay.foodromeo.R;
@@ -29,7 +22,7 @@ public class PaginationLayout extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.pagination);
+        setContentView(R.layout.view_pager);
 
         Toolbar tbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(tbar);
